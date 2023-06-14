7c9b9ef59aebceb83d0eaed0edb05f92e04a0908
Olga <olyab.715@gmail.com>
2023-06-14 22:53:01 +0500

    Change java-21

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13_676;
+        int mile_value = 20;
+        int miles = amount / mile_value;
+
+        System.out.println("Колличество начисленных миль: " + miles);
+
+    }
+}
